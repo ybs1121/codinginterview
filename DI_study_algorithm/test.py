41c7dec31d5116ca6d5e7d75de4389d0289a7383
YangByoungSeok <ybs991@naver.com>
2022-04-04 13:55:35 +0900

    백준 알고리즘, 2022.04.04

--- DI_study_algorithm/test.py
+++ DI_study_algorithm/test.py
@@ -1,22 +1,34 @@
-n, k = map(int, input().split())
+from itertools import *
+def solution(dist):
+    answer = []
 
-thing = [[0,0]]
-d = [[0]*(k+1) for _ in range(n+1)]
+    dist_max = 0
+    point = []
+    for i in range(len(dist)):
+        point.append(i)
 
-for i in range(n):
-    thing.append(list(map(int, input().split())))
+    for i in dist:
+        tmp = max(i)
+        if tmp > dist_max:
+            dist_max = tmp
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = thing[i][0]
-        v = thing[i][1]
+    printList = list(permutations(point, len(dist)))
 
-        if j < w:
-            d[i][j] = d[i-1][j]
-        else:
-            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
-    print(d)
-    print("------------------")
+    for i in printList:
+        sum = 0
+        i = list(i)
+        for j in range(len(dist)-1):
+            sum+=dist[i[j]][i[j+1]]
+        if sum == dist_max:
+            answer.append(i)
 
 
-print(d[n][k])
+
+
+    return answer
+
+
+dist = [[0,5,2,4,1],[5,0,3,9,6],[2,3,0,6,3],[4,9,6,0,3],[1,6,3,3,0]]
+result = [[1,2,0,4,3],[3,4,0,2,1]]
+
+print(solution(dist))
