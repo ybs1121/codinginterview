fd46356c476cee07e9c156b7297a4582a8a367b7
ybs <ybs>
2022-04-15 20:22:21 +0900

    BJ14889/04.15(Change)

--- DI_study_algorithm/백준_단기간에_성장/BJ14889.py
+++ DI_study_algorithm/백준_단기간에_성장/BJ14889.py
@@ -4,51 +4,69 @@ for i in range(n):
     tmp = list(map(int,input().split()))
     graph.append(tmp)
 
-# chk = [False] * n
-# kind = []
-# tmp = []
+chk = [False] * n
 
-#
-# def recur(num):
-#
-#     if num == n:
-#
-#         kind.append(tmp)
-#         return
-#
-#     for i in range(n):
-#         if chk[i] == False:
-#             chk[i] = True
-#             tmp.append(i)
-#             recur(num+1)
-#             chk[i] = False
-#             tmp.pop()
-#
-# recur(0)
+min_num = 9999
 
-tmp =[i for i in range(n)]
+tmp =[]
+kind = []
+def recur(num):
+    global min_num
 
+    if num == n:
+        team_a = tmp[0:num//2]
+        team_b = tmp[num//2:]
+        kind.append(tmp[:])
+        a = 0
+        b = 0
+        for j in team_a:
+            for k in team_a:
+                a += graph[j][k]
 
-from itertools import permutations
+        for j in team_b:
+            for k in team_b:
+                b += graph[j][k]
+        min_num = min(min_num,abs(a-b))
 
-min_num = 1000
-for i in permutations(tmp,n):
-    team_a = i[0:n//2]
-    team_b = i[n//2:]
-    a = 0
-    b = 0
-    for j in team_a:
-        for k in team_a:
-            a += graph[j][k]
-    for j in team_b:
-        for k in team_b:
-            b += graph[j][k]
+        return
 
-    tmp_num = abs(a - b)
+    for i in range(n):
+        if chk[i] == False:
+            chk[i] = True
+            tmp.append(i)
+            recur(num+1)
+            chk[i] = False
+            tmp.pop()
 
-    min_num = min(min_num, tmp_num)
+recur(0)
 
 print(min_num)
+
+print(kind)
+
+# tmp =[i for i in range(n)]
+#
+#
+# from itertools import permutations
+#
+# min_num = 1000
+# for i in permutations(tmp,n):
+#     team_a = i[0:n//2]
+#     team_b = i[n//2:]
+#     a = 0
+#     b = 0
+#     for j in team_a:
+#         for k in team_a:
+#             a += graph[j][k]
+#     for j in team_b:
+#         for k in team_b:
+#             b += graph[j][k]
+#
+#     tmp_num = abs(a - b)
+#
+#     min_num = min(min_num, tmp_num)
+#
+# print(min_num)
 
 
 
