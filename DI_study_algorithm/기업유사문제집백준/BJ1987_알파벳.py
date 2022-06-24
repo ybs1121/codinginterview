479885eadbfb7d5c00348e1b2bfcba10f8d05c7a
yangbyoungseok <ybs991@naver.com>
2022-06-24 16:38:03 +0900

    6/24

--- DI_study_algorithm/기업유사문제집백준/BJ1987_알파벳.py
+++ DI_study_algorithm/기업유사문제집백준/BJ1987_알파벳.py
@@ -1,62 +1,77 @@
+# import sys
+# from collections import deque
+#
+# input = sys.stdin.readline
+#
+# r, c= map(int,input().split())
+# maps = []
+#
+# for i in range(r):
+#     tmp = input().strip()
+#     tmp = list(tmp)
+#     maps.append(tmp)
+#
+# answer = 0
+#
+# def bfs():
+#     global answer
+#
+#     q = deque()
+#     q.append((0,0,1,['C']))
+#     zx = [1, 0, -1, 0]
+#     zy = [0, 1, 0, -1]
+#
+#     while q:
+#         x, y, count, charset = q.popleft()
+#         answer = max(count, answer)
+#
+#         for i in range(4):
+#             nx = x + zx[i]
+#             ny = y + zy[i]
+#             if 0 <= nx < r and 0 <= ny < c and maps[nx][ny] not in charset:
+#                 charset.append(maps[nx][ny])
+#                 q.append((nx,ny,count+1,charset))
+#                 charset.pop()
+#
+#
+#
+#
+#
+# bfs()
+# print(answer)
+
 import sys
-input = sys.stdin.readline
 
-r,c = map(int,input().split())
-maps = []
-for i in range(r):
-    tmp = list(input().rstrip())
-    maps.append(tmp)
-print(maps)
+# bfs 풀이
+# bfs
+def bfs():
+    global cnt
+    queue = set([(0, 0, graph[0][0])]) # 시간 초과를 줄이기 위해 중복되는 곳은 제거
 
-chk = set()
-answer = 0
-nx = [-1, 1, 0, 0]
-ny = [0, 0, 1, -1]
+    while queue:
+        x, y, z = queue.pop()
 
-def dfs(x,y,c):
-    global answer
-    answer = max(answer, c)
-    print(answer)
-    for i in range(4):
-        xx = x + nx[i]
-        yy = y + ny[i]
-        if 0 <= xx < r and 0 <= yy < c and  maps[xx][yy] in chk:
-            chk.add(maps[xx][yy])
-            dfs(xx, yy, c+1)
-            chk.remove(maps[xx][yy])
+        # 말이 지날 수 있는 최대의 칸 초기화
+        cnt = max(cnt, len(z))
 
-chk.add(maps[0][0])
-dfs(0,0,1)
+        # 상/하/좌/우 탐색
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-print("---------")
-print(answer)
-print("---------")
+            # 범위 내에 있고 알파벳이 중복이 안된다면 탐색
+            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in z:
+                queue.add((nx, ny, graph[nx][ny] + z))
 
 
+r, c = map(int, sys.stdin.readline().split())
 
-r, c = map(int, input().split())
-maps = []
-for _ in range(r):
-    maps.append(list(input().rstrip()))
-print(maps)
-ans = 0
-alphas = set()
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+graph = [list(map(str, sys.stdin.readline().strip())) for _ in range(r)]
 
-def dfs(x, y, count):
-    global ans
-    ans = max(ans, count)
-    print(ans)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and not maps[nx][ny] in alphas:
-            alphas.add(maps[nx][ny])
-            dfs(nx, ny, count+1)
-            alphas.remove(maps[nx][ny])
-alphas.add(maps[0][0])
-dfs(0, 0, 1)
-print(ans)
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+cnt = 1
 
+bfs()
+print(cnt)
 
